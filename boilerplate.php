d737a6b89904e47ec7c3731380fb6e66e0de24f4
aroy15 <anjonroy57@gmail.com>
2025-01-26 22:48:47 +0600

    Using the Data store inside the block

--- boilerplate.php
+++ boilerplate.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name:       Boilerplate
+ * Plugin Name:       Data Store Demo
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Anjon Roy
+ * Author URI:		  https://anjonroy.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       boilerplate
- *
+ * Text Domain:       data-store-demo
  * @package CreateBlock
  */
 
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-function create_block_boilerplate_block_init() {
+function data_store_demo_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'create_block_boilerplate_block_init' );
+add_action( 'init', 'data_store_demo_block_init' );
--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,19 @@
-import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import { useSelect, useDispatch } from '@wordpress/data';
 import './editor.scss';
 
 export default function Edit() {
+	const title = useSelect((select) => {
+		return select('core/editor').getEditedPostAttribute('title');
+	});
+	const { editPost } = useDispatch('core/editor');
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Boilerplate – hello from the editor!', 'boilerplate' ) }
-		</p>
+		<div {...useBlockProps()}>
+			<h2>{title}</h2>
+			<input
+				value={title}
+				onChange={(e) => editPost({ title: e.target.value })}
+			/>
+		</div>
 	);
 }
